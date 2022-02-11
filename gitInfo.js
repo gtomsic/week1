f1cc331714908c8888334c250bddb20efe4471fd
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-11 10:58:46 -0700

    Problem 2 Git Info done

--- gitInfo.js
+++ gitInfo.js
@@ -10,6 +10,8 @@
 */
 
 //CODE HERE
+const gitDefinition =
+  'Git is a version control system, that allow us to create, manage, update and create a snap shot of our work and easy way to upload our work to any server that support git version control';
 
 //////////////////PROBLEM 2////////////////////
 /*
@@ -18,6 +20,8 @@
 */
 
 //CODE HERE
+const gitHubDefinition =
+  'Github is a server where we can save data and organize data base on the project, And Github also support git version control systen';
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -26,6 +30,8 @@
 */
 
 //CODE HERE
+const gitInitDefinition =
+  'Git init is the first step to be done to allow us managing our work or files';
 
 //////////////////PROBLEM 4////////////////////
 /*
@@ -34,6 +40,8 @@
 */
 
 //CODE HERE
+const gitCloneDefinition =
+  'Git clone is the comman for clonning the projects that is already existing.';
 
 //////////////////PROBLEM 5////////////////////
 /*
@@ -42,6 +50,8 @@
 */
 
 //CODE HERE
+const gitStatusDefinition =
+  'Git status is for us to check or monitor the changes in our project or files etc';
 
 //////////////////PROBLEM 6////////////////////
 /*
@@ -53,6 +63,8 @@
 */
 
 //CODE HERE
+const gitAddDefinition = 'Git add is to add file contents to the index';
+const gitAddCode = 'Git add code is to prepare our code before we commit';
 
 //////////////////PROBLEM 7////////////////////
 /*
@@ -64,7 +76,10 @@
 */
 
 //CODE HERE
-
+const gitCommitDefinition =
+  'Git commit is to record the changes to the repository';
+const gitCommitCode =
+  'Git commit where we pack our code and ready to push to the server, I prefer it backup';
 
 //////////////////PROBLEM 8////////////////////
 /*
@@ -73,3 +88,5 @@
 */
 
 //CODE HERE
+const gitPushDefinition =
+  'Git push is we finaly goin to push our project to server || backup || production  who support git version control';
