f5ec37269a691b04451dea17738b65823446d6bd
Gabriel Tomsic <tomsicgabriel@gmail.com>
2022-02-11 10:37:05 -0700

    Array Problem done

--- arrays.js
+++ arrays.js
@@ -1,14 +1,17 @@
 //////////////////PROBLEM 1////////////////////
 /* Create a copy of the faveColors array called 'colorCopy' using the slice method. */
 
-const faveColors = ['red', 'green', 'black']
+const faveColors = ['red', 'green', 'black'];
 
 //CODE HERE
-
+const colorCopy = faveColors.slice(0, 3);
+console.log('Check Value colorCopy: ', colorCopy);
 //////////////////PROBLEM 2////////////////////
 /* Add a fourth color to the end of the 'colorCopy' array using the push method. */
 
 //CODE HERE
+colorCopy.push('blue');
+console.log('Check Value colorCopy: ', colorCopy);
 
 //////////////////PROBLEM 3////////////////////
 /*
@@ -16,10 +19,11 @@ const faveColors = ['red', 'green', 'black']
   numbers array that will capture only the middle numbers (2, 3, 4).
 */
 
-const numbers = [1, 2, 3, 4, 5]
+const numbers = [1, 2, 3, 4, 5];
 
 //CODE HERE
-
+const middleNums = numbers.slice(1, 4);
+console.log('Middle Numbers: ', middleNums);
 
 //////////////////PROBLEM 4////////////////////
 /* 
@@ -31,6 +35,17 @@ const numbers = [1, 2, 3, 4, 5]
   to the answers array. 
 */
 
-const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352]
+const bigOrSmallArray = [1, 99, 42, 69, 102, -10, 159, 352];
+const answers = [];
 
 // CODE HERE
+for (let i = 0; i < bigOrSmallArray.length; i++) {
+  if (bigOrSmallArray[i] > 100) {
+    answers.push(`${bigOrSmallArray[i]}`);
+  } else if (bigOrSmallArray[i] <= 100) {
+    answers.push(`${bigOrSmallArray[i]}`);
+  }
+}
+
+console.log('Bir or small array: ', bigOrSmallArray);
+console.log('Problem answers: ', answers);
